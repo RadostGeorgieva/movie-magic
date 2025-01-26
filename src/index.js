74b1a23f42123c7d1256e4c8ec5bf05a137bbee4
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-01-26 12:18:16 +0200

    install and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set ('views', 'src/views')
+
 app.get('/', (req, res) => {
 
-    res.send("It works");
+    res.render('home', {layout:false })
 
 })
 
