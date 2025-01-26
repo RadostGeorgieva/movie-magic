1fa7bb99bac6216eaac0a14f4b3a2f555a419beb
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-01-26 12:46:36 +0200

    added create view

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ app.get('/about', (req, res) => {
     res.render('about')
 
 });
+app.get('/create', (req, res) => {
+
+    res.render('create')
+
+});
+});
 app.get('/*', (req, res) => {
 
     res.render('404')
