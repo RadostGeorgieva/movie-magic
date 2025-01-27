b075b27faff1506a9c7128e545c73ab48d9143f7
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-01-27 20:24:07 +0200

    add routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import homeController from './controllers/home-controller.js'
+import routes from './routes.js';
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.set ('views', 'src/views')
 
 app.use('/static', express.static('./src/public'))
 
-app.use(homeController)
+app.use(routes);
 
 
 app.get('/create', (req, res) => {
@@ -36,11 +37,7 @@ app.get('/search', (req, res) => {
     res.render('search')
 
 });
-app.get('*', (req, res) => {
 
-    res.render('404')
-
-});
 
 
 
