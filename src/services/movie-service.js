3014dc4401d1a8e945e9cad2724c7d69d5d390e1
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-02-03 13:57:52 +0200

    refactgor search page

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,25 +1,27 @@
 
-import {v4 as uuid} from 'uuid'
 import Movie from "../models/Movie.js";
 
 export default {
-   async findAll(filter = {}) {
-        // if(filter.search) {
-        //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-        // }
+    async findAll(filter = {}) {
 
-        // if(filter.genre) {
-        //     result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
-        // }
+        let query = Movie.find({});;
 
-        // if(filter.year) {
-        //     result = result.filter(movie => movie.year.toLowerCase() === filter.year);
-        // }
-        return Movie.find({});
+        if (filter.search) {
+            query = query.where({ title: filter.search })
+        }
+
+        if (filter.genre) {
+            query = query.where({ genre: filter.genre })
+        }
+
+        if (filter.year) {
+            query = query.where({ year: Number(filter.year) })
+        }
+        return query
     },
 
     findOne(movieId) {
-        
+
         // TODO: if movie is missing?
         const result = Movie.findById(movieId)
 
@@ -29,9 +31,8 @@ export default {
     create(movieData) {
         const newId = uuid();
         movies.push({
-            id:newId,
             ...movieData,
-            rating:Number(movieData.rating),
+            rating: Number(movieData.rating),
         });
 
         return newId;
