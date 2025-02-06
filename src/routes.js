af37efc24269078c729483cbf1c32426e4c406f1
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-02-06 16:32:45 +0200

    fixed resources

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,19 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import homeController from './controllers/home-controller.js'
-import movieController from './controllers/movie-controller.js'
-import castController from "./controllers/cast-controller.js";
-import authController from "./controllers/auth-controller.js";
+import homeController from './controllers/home-controller.js';
+import movieController from './controllers/movie-controller.js';
+import castController from './controllers/cast-controller.js';
+import authController from './controllers/auth-controller.js';
 
 const routes = Router();
 
 routes.use(homeController);
 routes.use('/movies', movieController);
-routes.use('/casts', castController)
-routes.use('/auth', authController)
+routes.use('/casts', castController);
+routes.use('/auth', authController);
 
 routes.get('*', (req, res) => {
-
-    res.render('404')
-
+    res.render('404');
 });
 
-export default routes
+export default routes;
