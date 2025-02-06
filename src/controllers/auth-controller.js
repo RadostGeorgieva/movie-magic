c02b81030b196e9832dcbb8d556ba318dd02aa73
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-02-06 15:01:47 +0200

    send data to register endpoint

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,4 +6,10 @@ const authController = Router();
 authController.get('/register', (req, res) => {
     res.render('auth/register')
 })
+
+authController.post('/register', (req, res) => {
+    const userData = req.body;
+
+    res.end();
+})
 export default authController;
