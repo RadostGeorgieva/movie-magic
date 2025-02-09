9a345a7e138754b1e28e85c181eccb628651c13e
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2025-02-09 19:47:32 +0200

    show details buttons if user is creator

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -28,14 +28,15 @@ movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOneWithCasts(movieId);
     // const casts = await castService.getAll(movie.casts);
+    const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
 
-    res.render('movie/details', { movie });
+    res.render('movie/details', { movie, isCreator });
 });
 
 movieController.get('/:movieId/attach-cast', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.findOne(movieId)
-    const casts = await castService.getAll({exclude: movie.casts});
+    const casts = await castService.getAll({ exclude: movie.casts });
 
     res.render('movie/attach-cast', { movie, casts });
 });
